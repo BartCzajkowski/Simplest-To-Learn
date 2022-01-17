e20884996e85dceb0787b10b7e00a252fe141318
Bartosz Czajkowski <88278871+BartCzajkowski@users.noreply.github.com>
2022-01-17 14:34:12 +0100

    Update kolko i krzyzyk.py
    Usunięte niepotrzebne importy i kilka niepotrzebnych spacji, linijek

--- kolko i krzyzyk.py
+++ kolko i krzyzyk.py
@@ -2,9 +2,8 @@
 Lista aplikacji 
 '''
 from tkinter import *
-from tkinter import ttk
 from tkinter import messagebox
-from os import startfile
+
 #Początek aplikacji
 root = Tk()
 root.title('Kółko i Krzyżyk')
@@ -13,7 +12,6 @@ root.title('Kółko i Krzyżyk')
 iks = True
 licznik = 0
 #Funkcje
-
 
 def koniec_gry():
     b1.config(state=DISABLED)
@@ -36,6 +34,7 @@ def wygrana():
         wygrany = True
         messagebox.showinfo('Kółko i krzyżyk', 'Gracz X wygrywa!')
         koniec_gry()
+        
     elif b4['text'] == 'X' and b5['text'] == 'X' and b6['text'] == 'X':
         b4.config(bg='green')
         b5.config(bg='green')
@@ -43,6 +42,7 @@ def wygrana():
         wygrany = True
         messagebox.showinfo('Kółko i krzyżyk', 'Gracz X wygrywa!')
         koniec_gry()
+        
     elif b7['text'] == 'X' and b8['text'] == 'X' and b9['text'] == 'X':
         b7.config(bg='green')
         b8.config(bg='green')
@@ -50,6 +50,7 @@ def wygrana():
         wygrany = True
         messagebox.showinfo('Kółko i krzyżyk', 'Gracz X wygrywa!')
         koniec_gry()
+        
     elif b1['text'] == 'X' and b4['text'] == 'X' and b7['text'] == 'X':
         b1.config(bg='green')
         b4.config(bg='green')
@@ -57,6 +58,7 @@ def wygrana():
         wygrany = True
         messagebox.showinfo('Kółko i krzyżyk', 'Gracz X wygrywa!')
         koniec_gry()
+        
     elif b2['text'] == 'X' and b5['text'] == 'X' and b8['text'] == 'X':
         b2.config(bg='green')
         b5.config(bg='green')
@@ -64,6 +66,7 @@ def wygrana():
         wygrany = True
         messagebox.showinfo('Kółko i krzyżyk', 'Gracz X wygrywa!')
         koniec_gry()
+        
     elif b3['text'] == 'X' and b6['text'] == 'X' and b9['text'] == 'X':
         b3.config(bg='green')
         b6.config(bg='green')
@@ -71,6 +74,7 @@ def wygrana():
         wygrany = True
         messagebox.showinfo('Kółko i krzyżyk', 'Gracz X wygrywa!')
         koniec_gry()
+        
     elif b1['text'] == 'X' and b5['text'] == 'X' and b9['text'] == 'X':
         b1.config(bg='green')
         b5.config(bg='green')
@@ -78,6 +82,7 @@ def wygrana():
         wygrany = True
         messagebox.showinfo('Kółko i krzyżyk', 'Gracz X wygrywa!')
         koniec_gry()
+        
     elif b3['text'] == 'X' and b5['text'] == 'X' and b7['text'] == 'X':
         b3.config(bg='green')
         b5.config(bg='green')
@@ -85,7 +90,7 @@ def wygrana():
         wygrany = True
         messagebox.showinfo('Kółko i krzyżyk', 'Gracz X wygrywa!')
         koniec_gry()
-
+        
     elif b1['text'] == 'O' and b2['text'] == 'O' and b3['text'] == 'O':
         b1.config(bg='green')
         b2.config(bg='green')
@@ -93,6 +98,7 @@ def wygrana():
         wygrany = True
         messagebox.showinfo('Kółko i krzyżyk', 'Gracz O wygrywa!')
         koniec_gry()
+        
     elif b4['text'] == 'O' and b5['text'] == 'O' and b6['text'] == 'O':
         b4.config(bg='green')
         b5.config(bg='green')
@@ -100,6 +106,7 @@ def wygrana():
         wygrany = True
         messagebox.showinfo('Kółko i krzyżyk', 'Gracz O wygrywa!')
         koniec_gry()
+        
     elif b7['text'] == 'O' and b8['text'] == 'O' and b9['text'] == 'O':
         b7.config(bg='green')
         b8.config(bg='green')
@@ -107,6 +114,7 @@ def wygrana():
         wygrany = True
         messagebox.showinfo('Kółko i krzyżyk', 'Gracz O wygrywa!')
         koniec_gry()
+        
     elif b1['text'] == 'O' and b4['text'] == 'O' and b7['text'] == 'O':
         b1.config(bg='green')
         b4.config(bg='green')
@@ -114,6 +122,7 @@ def wygrana():
         wygrany = True
         messagebox.showinfo('Kółko i krzyżyk', 'Gracz O wygrywa!')
         koniec_gry()
+        
     elif b2['text'] == 'O' and b5['text'] == 'O' and b8['text'] == 'O':
         b2.config(bg='green')
         b5.config(bg='green')
@@ -121,6 +130,7 @@ def wygrana():
         wygrany = True
         messagebox.showinfo('Kółko i krzyżyk', 'Gracz O wygrywa!')
         koniec_gry()
+        
     elif b3['text'] == 'O' and b6['text'] == 'O' and b9['text'] == 'O':
         b3.config(bg='green')
         b6.config(bg='green')
@@ -128,6 +138,7 @@ def wygrana():
         wygrany = True
         messagebox.showinfo('Kółko i krzyżyk', 'Gracz O wygrywa!')
         koniec_gry()
+        
     elif b1['text'] == 'O' and b5['text'] == 'O' and b9['text'] == 'O':
         b1.config(bg='green')
         b5.config(bg='green')
@@ -135,6 +146,7 @@ def wygrana():
         wygrany = True
         messagebox.showinfo('Kółko i krzyżyk', 'Gracz O wygrywa!')
         koniec_gry()
+        
     elif b3['text'] == 'O' and b5['text'] == 'O' and b7['text'] == 'O':
         b3.config(bg='green')
         b5.config(bg='green')
@@ -142,7 +154,8 @@ def wygrana():
         wygrany = True
         messagebox.showinfo('Kółko i krzyżyk', 'Gracz O wygrywa!')
         koniec_gry()
-    if licznik == 9 and wygrany is False:
+        
+    elif licznik == 9 and wygrany is False:
         messagebox.showinfo('Kółko i krzyżyk', 'Remis!')
         koniec_gry()
 
@@ -173,11 +186,9 @@ def restart():
     b1 = Button(root, text='', font=(20), height = 5, width=10, bg='SystemButtonFace', command=lambda: b_click(b1) )
     b2 = Button(root, text='', font=(20), height = 5, width=10, bg='SystemButtonFace', command=lambda: b_click(b2) )
     b3 = Button(root, text='', font=(20), height = 5, width=10, bg='SystemButtonFace', command=lambda: b_click(b3) )
-
     b4 = Button(root, text='', font=(20), height = 5, width=10, bg='SystemButtonFace', command=lambda: b_click(b4) )
     b5 = Button(root, text='', font=(20), height = 5, width=10, bg='SystemButtonFace', command=lambda: b_click(b5) )
     b6 = Button(root, text='', font=(20), height = 5, width=10, bg='SystemButtonFace', command=lambda: b_click(b6) )
-
     b7 = Button(root, text='', font=(20), height = 5, width=10, bg='SystemButtonFace', command=lambda: b_click(b7) )
     b8 = Button(root, text='', font=(20), height = 5, width=10, bg='SystemButtonFace', command=lambda: b_click(b8) )
     b9 = Button(root, text='', font=(20), height = 5, width=10, bg='SystemButtonFace', command=lambda: b_click(b9) )
@@ -187,11 +198,9 @@ def restart():
     b1.grid(row=0, column=0)
     b2.grid(row=0, column=1)
     b3.grid(row=0, column=2)
-
     b4.grid(row=1, column=0)
     b5.grid(row=1, column=1)
     b6.grid(row=1, column=2)
-
     b7.grid(row=2, column=0)
     b8.grid(row=2, column=1)
     b9.grid(row=2, column=2)
